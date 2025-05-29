de0752e3f8e88d3a3748b6e3ffa3540f4a476939
Vahe Manukyan <maanukyaan@gmail.com>
2025-05-29 23:12:43 +0400

    Mobile styles, mobile home background video

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,23 +7,23 @@ export default function Home() {
     <div className="relative h-full w-full">
       <Header />
 
-      <main className="relative z-10 flex h-full flex-col p-10 pt-40">
-        <h1 className="font-title text-6xl leading-tight font-semibold tracking-wide uppercase">
+      <main className="relative z-10 flex h-full flex-col p-10 px-5 pt-40 lg:px-10">
+        <h1 className="font-title text-2xl leading-tight font-semibold tracking-wide uppercase lg:text-6xl">
           Tired of just <i className="font-light line-through">listening</i>
           <i>?</i> <br />
           Maybe it's time to <b className="font-black">see</b> it.
         </h1>
-        <h2 className="font-text mt-5 mb-8 pl-1 text-lg font-light tracking-wide">
+        <h2 className="font-text mt-5 mb-8 pl-1 text-sm font-light tracking-wide lg:text-lg">
           Some feelings can't be heard. Only seen.
           <br />
           What if your emotions had shape and color?
         </h2>
 
         <Link to="/app" className="w-fit">
-          <Button>Experience it</Button>
+          <Button className="text-xs lg:text-base">Experience it</Button>
         </Link>
 
-        <h3 className="absolute bottom-10 left-10 max-w-[45%] font-mono leading-relaxed text-[#ccc]">
+        <h3 className="absolute bottom-5 left-5 max-w-[85%] font-mono text-xs leading-relaxed text-[#ccc] lg:bottom-10 lg:left-10 lg:max-w-[45%] lg:text-base">
           <strong className="font-orbitron">Synaesthesia</strong> is a rare
           phenomenon where one sense involuntarily triggers another — like{" "}
           <em>seeing color when hearing music</em>. It's not imagination — it's
@@ -34,13 +34,23 @@ export default function Home() {
         </h3>
 
         <video
-          className="pointer-events-none absolute top-0 left-0 z-[-1] h-full w-full object-cover opacity-70"
+          className="pointer-events-none absolute top-0 left-0 z-[-1] hidden h-full w-full object-cover opacity-70 sm:block"
           autoPlay
           loop
           muted
+          playsInline
         >
           <source src="/home_background.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
+        </video>
+
+        <video
+          className="pointer-events-none absolute top-0 left-0 z-[-1] block h-full w-full object-cover opacity-70 sm:hidden"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src="/home_background_mobile.mp4" type="video/mp4" />
         </video>
       </main>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 z-[9999] flex w-full items-center justify-between rounded-b-lg border-b border-white/15 px-10 py-5 backdrop-blur-md">
+    <header className="fixed top-0 left-0 z-[9999] flex w-full items-center justify-between rounded-b-lg border-b border-white/15 px-5 lg:px-10 py-3 lg:py-5 backdrop-blur-md">
       <div className="logo flex items-center">
-        <img src="/logo.svg" alt="Logo" className="size-10" />
-        <p className="font-orbitron ml-3 text-lg uppercase select-none">
+        <img src="/logo.svg" alt="Logo" className="size-8 lg:size-10" />
+        <p className="font-orbitron ml-2 lg:ml-3 lg:text-lg uppercase select-none">
           Syntaesthesia
         </p>
       </div>
